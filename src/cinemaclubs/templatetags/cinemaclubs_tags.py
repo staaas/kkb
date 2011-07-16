8bc25f4818bef52a29dcc02073c8d024a7106f0e
Stas Rudakou <dev@nott.cc>
2011-07-16 18:41:15 +0300

    Modified comments/form.html template

--- src/cinemaclubs/templatetags/cinemaclubs_tags.py
+++ src/cinemaclubs/templatetags/cinemaclubs_tags.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('cinemaclubs/tags/comments.html')
-def custom_comments_tree(obj):
-    return {'obj': obj}
+@register.inclusion_tag('cinemaclubs/tags/comments.html', takes_context=True)
+def custom_comments_tree(context, obj):
+    return {'obj': obj,
+            'request': context['request']}
