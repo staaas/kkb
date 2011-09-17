692263357c552f791dac2daea7ac89d140b30e98
Stas Rudakou <dev@nott.cc>
2011-09-18 00:50:17 +0300

    Improve admin interface

--- src/cinemaclubs/admin.py
+++ src/cinemaclubs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
+
 from models import CinemaClub, CinemaClubEvent
 
 class CinemaClubAdmin(admin.ModelAdmin):
@@ -6,6 +8,28 @@ class CinemaClubAdmin(admin.ModelAdmin):
 admin.site.register(CinemaClub, CinemaClubAdmin)
 
 class CinemaClubEventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'organizer', 'starts_at', 'published']
+    list_filter = ['organizer']
+
+    fieldsets = (
+        (None, {'fields': ('name', 'organizer', 'short_description',
+                           'description', 'published')}),
+        (_('Scheduling'), {'fields': ('starts_at', 'ends_at')}),
+        (_('Images'), {'fields': ('poster',)}),
+    )
+
+    actions = ['action_publish', 'action_unpublish']
+    actions_selection_counter = False
+
+    #date_hierarchy = 'starts_at'
+
+    def action_publish(self, request, queryset):
+        queryset.update(published=True)
+    action_publish.short_description = _(u'Publish')
+
+    def action_unpublish(self, request, queryset):
+        queryset.update(published=False)
+    action_unpublish.short_description = _(u'Unpublish')
+
 admin.site.register(CinemaClubEvent, CinemaClubEventAdmin)
 
