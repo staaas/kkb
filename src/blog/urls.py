d067a7fe12b9beb2ae8765a2d8f67b1fc140b6c2
Stas Rudakou <dev@nott.cc>
2011-09-21 18:23:08 +0300

    Add RSS and ATOM feeds to head

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,7 +5,7 @@ import feeds
 urlpatterns = patterns('blog.views',
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
         'post_detail', name="blog_post_detail"),
-    (r'^rss/$', feeds.LatestEntriesRss()),
-    (r'^atom/$', feeds.LatestEntriesAtom()),
+    url(r'^rss/$', feeds.LatestEntriesRss(), name='blog_feed_rss'),
+    url(r'^atom/$', feeds.LatestEntriesAtom(), name='blog_feed_atom'),
     url(r'^$', 'posts', name='blog_posts'),
 )
